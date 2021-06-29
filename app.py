f00927f8c571ab75cd24efe2d8d97cc1c2f7b3b6
Suphapol Wongsawat <suphapol.2545@gmail.com>
2021-06-30 00:57:34 +0700

    changed the date

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import datetime
 # ใส่ title ของ sidebar
 st.sidebar.write('## KBANK.BKK stock predictor')
 st.sidebar.write('### Enter first and last date for prediction for KBANK.BKK stock')
-first_date = st.sidebar.date_input('First date', datetime.date(2010,6,27))
+first_date = st.sidebar.date_input('First date', datetime.date(2020,6,27))
 last_date = st.sidebar.date_input('Last date', datetime.date(2021,6,27))
 st.sidebar.write('If you want a predicted price of today\'s KBANK stock enter the current date.')
 
